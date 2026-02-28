44e1f135b842c6d526cd9827b8ea1ab3a77c746e
Jehanzeb Ali <jehanzebsherali@gmail.cmo>
2026-02-28 14:59:44 +0800

    no message

--- platform/themes/resido/partials/shortcodes/properties-by-neighbourhood.blade.php
+++ platform/themes/resido/partials/shortcodes/properties-by-neighbourhood.blade.php
@@ -28,7 +28,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-7 col-md-10 text-center">
                 <div class="sec-heading center">
-                    <h2>{!! clean($title) !!} {{$countryId}}</h2>
+                    <h2>{!! clean($title) !!}</h2>
                     <p>{!! clean($description) !!}</p>
                 </div>
             </div>
