ec3ca7f3a028e79cc3577f2f23a22414ab6c3f02
Jehanzeb Ali <jehanzebsherali@gmail.cmo>
2026-03-01 17:20:51 +0800

    no message

--- platform/themes/resido/partials/real-estate/filters/type.blade.php
+++ platform/themes/resido/partials/real-estate/filters/type.blade.php
@@ -9,7 +9,7 @@
     @endphp
     <div class="d-flex flex-row flex-nowrap">
         @foreach ($types as $key => $type)
-            <div class="form-check form-check-inline">
+            <div class="">
                 <input class="btn-check d-none" type="radio" name="type" id="cp-{{ $key }}" value="{{ $key }}"
                        @if ($type_choice == $key) checked @endif autocomplete="off">
                 <label class="btn btn-outline-dark" for="cp-{{ $key }}">{{ $type }}</label>
