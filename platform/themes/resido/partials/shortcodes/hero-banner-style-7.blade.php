21fde013d6781f157ad4c339f221d786010d0a39
Jehanzeb Ali <jehanzebsherali@gmail.cmo>
2026-03-01 12:51:10 +0800

    no message

--- platform/themes/resido/partials/shortcodes/hero-banner-style-7.blade.php
+++ platform/themes/resido/partials/shortcodes/hero-banner-style-7.blade.php
@@ -4,7 +4,7 @@
     $videoExtensions = ['mp4','webm','mov','avi','mkv'];
 @endphp
 
-<div style="height: 80vh; position: relative; overflow: hidden;">
+<div class="hero-banner" style="height: 80vh; position: relative; overflow: hidden;">
 
     @if(in_array($extension, $videoExtensions))
 
@@ -46,73 +46,75 @@
         z-index:2;
         max-width:50%;
     ">
-        <h1 style="margin-bottom: 10px;font-size: 3.75rem;color:#fff;">
+        <h1 class="hero-title">
             {{ $title }}
         </h1>
-        <p>
+        <p class="hero-desc">
             {{ $description }}
         </p>
     </div>
 
-</div>
+    <!-- Search Form Overlay -->
+    <div class="hero-form-wrapper">
+        <div class="container">
+            <form action="{{ route('public.properties') }}" method="GET" id="frmhomesearch">
+                <div class="hero-search-content form-overlay-box">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12">
+                            <div class="form-group">
+                                <div class="choose-property-type">
+                                    {!! Theme::partial('real-estate.filters.type') !!}
+                                </div>
+                            </div>
+                        </div>
 
-<section>
-    <div class="container">
-        <form action="{{ route('public.properties') }}" method="GET" id="frmhomesearch">
-            <div class="hero-search-content">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                        <div class="form-group">
-                            <div class="choose-property-type">
-                                {!! Theme::partial('real-estate.filters.type') !!}
+                        <div class="col-lg-4 col-md-4 col-sm-12">
+                            <div class="form-group">
+                                <div class="input-with-icon">
+                                    {!! Theme::partial('real-estate.filters.input-search') !!}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-4 col-sm-12">
+                            <div class="form-group borders">
+                                <div class="input-with-icon">
+                                    {!! Theme::partial('real-estate.filters.categories') !!}
+                                    <i class="ti-briefcase"></i>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-12">
+                            <div class="form-group mb-2">
+                                <div class="input-with-icon">
+                                    {!! Theme::partial('real-estate.filters.cities') !!}
+                                    <i class="ti-location-pin"></i>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="form-group simple mb-2">
+                                {!! Theme::partial('real-estate.filters.min-price') !!}
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-12">
+                            <div class="form-group simple mb-2">
+                                {!! Theme::partial('real-estate.filters.max-price') !!}
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-3 col-sm-12">
+                            <div class="form-group">
+                                <button class="btn search-btn" type="submit">{{ __('Search') }}</button>
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                        <div class="form-group">
-                            <div class="input-with-icon">
-                                {!! Theme::partial('real-estate.filters.input-search') !!}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                        <div class="form-group borders">
-                            <div class="input-with-icon">
-                                {!! Theme::partial('real-estate.filters.categories') !!}
-                                <i class="ti-briefcase"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12">
-                        <div class="form-group mb-2">
-                            <div class="input-with-icon">
-                                {!! Theme::partial('real-estate.filters.cities') !!}
-                                <i class="ti-location-pin"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <div class="form-group simple mb-2">
-                            {!! Theme::partial('real-estate.filters.min-price') !!}
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-12">
-                        <div class="form-group simple mb-2">
-                            {!! Theme::partial('real-estate.filters.max-price') !!}
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12">
-                        <div class="form-group">
-                            <button class="btn search-btn" type="submit">{{ __('Search') }}</button>
-                        </div>
-                    </div>
-
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</section>
 
-<!-- ============================ Hero Banner End ================================== -->
+</div>
