19b7a0fe139e73286a543fe3a4162d969a47cb93
Jehanzeb Ali <jehanzebsherali@gmail.cmo>
2026-03-06 04:20:50 +0800

    no message

--- platform/themes/resido/partials/real-estate/filters/type.blade.php
+++ platform/themes/resido/partials/real-estate/filters/type.blade.php
@@ -5,15 +5,17 @@
 
 @if (count($types))
     @php
-        $type_choice = request()->input('type', PropertyTypeEnum::SALE());
+        $type_choice = request()->input('type', PropertyTypeEnum::OFFPLAN());
     @endphp
     <div class="d-flex flex-row flex-nowrap">
         @foreach ($types as $key => $type)
+            @if($key == 'offplan')
             <div class="">
                 <input class="btn-check d-none" type="radio" name="type" id="cp-{{ $key }}" value="{{ $key }}"
                        @if ($type_choice == $key) checked @endif autocomplete="off">
                 <label class="btn btn-outline-dark" for="cp-{{ $key }}">{{ $type }}</label>
             </div>
+                @endif
         @endforeach
     </div>
 @endif
